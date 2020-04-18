04c8300530efa2fef57329a2da6982d57b917211
Mouhamed Wade <mdwade37@gmail.com>
2020-04-18 11:16:11 +0000

    video demo

--- src/app/compte/incription/incription.page.ts
+++ src/app/compte/incription/incription.page.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {AuthService} from '../../services/auth.service';
 import {Router} from '@angular/router';
-import {User} from "../../models/user";
-import {LoadingController} from "@ionic/angular";
+import {User} from '../../models/user';
+import {LoadingController} from '@ionic/angular';
 
 @Component({
   selector: 'app-incription',
